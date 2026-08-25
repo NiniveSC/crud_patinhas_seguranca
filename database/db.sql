64f17e622b9b108bd0e0f75cac669bb3f74aa9af
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-25 10:50:51 -0300

    add - fiz o banco de dados com as duas tabelas e a chave primaria e estrangeira

---
+++ database/db.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE sistema_patinhas_seguranca;
+USE sistema_patinhas_seguranca;
+
+CREATE TABLE cliente (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome_cliente VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE animal (
+    id_animal INT AUTO_INCREMENT PRIMARY KEY,
+    tipo_animal VARCHAR(100) NOT NULL,
+    raca_animal VARCHAR(100) NOT NULL,
+    idade_animal DATE NOT NULL,
+    cliente_id INT NOT NULL,
+    FOREIGN KEY (cliente_id) REFERENCES cliente(id_cliente)
+);
