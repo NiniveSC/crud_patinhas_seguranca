d67cf45b0038a1b8fc60faaf9d941603fc6bdb58
Nínive <ninive_cararo@estudante.sesisenai.org.br>
2026-08-26 12:05:07 -0300

    add- fiz o index

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include "infra/conexao.php";
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <title>AUmigos Pet Shop</title>
+</head>
+
+<body>
+
+    <h1>Bem vindo! O que deseja fazer hoje?</h1>
+
+    <a href="public/cadastrar_cliente.php">
+        Cadastrar Cliente
+    </a>
+
+    <a href="public/cadastrar_animais.php">
+        Cadastrar Animal
+    </a>
+
+</body>
+
+</html>
